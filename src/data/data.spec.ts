c2a30f00e97f804baadcd29776ea068d2ce93810
Reto Hablützel <rethab@protonmail.ch>
2021-05-23 10:03:17 +0200

    ensure no dangling data

--- src/data/data.spec.ts
+++ src/data/data.spec.ts
@@ -28,3 +28,40 @@ test("episodes are linked to existing podcasts", () => {
     );
   });
 });
+
+test("all people are referenced", () => {
+  const referencedPeople = collectUniqueIds(quotes, (q) => [
+    q.quoteById,
+    q.quotedById,
+  ]);
+  people.forEach((p) => {
+    expect(referencedPeople).toEqual(expect.arrayContaining([p.id]));
+  });
+});
+
+test("all podcasts are referenced", () => {
+  const referencedPodcasts = collectUniqueIds(episodes, (e) => [e.podcastId]);
+  podcasts.forEach((p) => {
+    expect(referencedPodcasts).toEqual(expect.arrayContaining([p.id]));
+  });
+});
+
+test("all episodes are referenced", () => {
+  const referencedEpisodes = collectUniqueIds(quotes, (q) => [q.episodeId]);
+  episodes.forEach((e) => {
+    expect(referencedEpisodes).toEqual(expect.arrayContaining([e.id]));
+  });
+});
+
+function collectUniqueIds<T>(
+  elems: Array<T>,
+  getIds: (obj: T) => Array<number | undefined>
+): Array<number> {
+  const result: Set<number> = new Set();
+  elems.forEach((e) => {
+    getIds(e).forEach((id) => {
+      if (id) result.add(id);
+    });
+  });
+  return Array.from(result);
+}
